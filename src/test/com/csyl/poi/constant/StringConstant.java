d2fe45e17931edb37d34e3e74fd4145286c0f695
HLhhh <1726893505@qq.com>
2021-08-25 23:50:28 +0800

    feat(csv): 分类去重

---
+++ src/test/com/csyl/poi/constant/StringConstant.java
@@ -0,0 +1,5 @@
+package com.csyl.poi.constant;
+
+public class StringConstant {
+    public final static String SPACE = " ";
+}
--- src/test/com/csyl/poi/excel/_csv_test.java
+++ src/test/com/csyl/poi/excel/_csv_test.java
@@ -1,11 +1,26 @@
 package com.csyl.poi.excel;
 
 import com.csvreader.CsvReader;
+import com.csyl.poi.dto.CsvDataDTO;
+import com.csyl.poi.dto.DataMatch;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.charset.Charset;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 public class _csv_test {
@@ -24,20 +39,101 @@ public class _csv_test {
         Arrays.stream(headers).map(a -> a + ":").forEach(System.out::print);
         System.out.println();
 
+
         // 读取每行的内容
+        List<CsvDataDTO> csvDataDTOList = new ArrayList<>();
         while (csvReader.readRecord()) {
-            String collect = Arrays.stream(headers).map(a -> {
+            CsvDataDTO csvDataDTO = new CsvDataDTO();
+            Map<String, Field> map = getStringFieldMap(csvDataDTO);
+
+            String collect = Arrays.stream(headers).map(name -> {
                 try {
-                    return csvReader.get(a);
-                } catch (IOException e) {
+                    String value = csvReader.get(name);
+                    if (map.get(name) != null) {
+                        map.get(name).setAccessible(true);
+                        map.get(name).set(csvDataDTO, value);
+                    }
+                    return value;
+                } catch (IOException | IllegalAccessException e) {
                     e.printStackTrace();
                 }
                 return "Null";
             }).collect(Collectors.joining(":"));
+            csvDataDTOList.add(csvDataDTO);
             System.out.println(collect);
         }
 
+        csvDataDTOList.forEach(CsvDataDTO::_2DateTime);
+        HashMap<String, List<CsvDataDTO>> collect = csvDataDTOList.stream()
+                .collect(Collectors.groupingBy(
+                        CsvDataDTO::getSpecies,
+                        HashMap::new,
+                        Collectors.toList()));
+
+
+        List<CsvDataSort> resultList = new ArrayList<>();
+        for (Map.Entry<String, List<CsvDataDTO>> entry : collect.entrySet()) {
+            //物种
+            String key = entry.getKey();
+            AtomicReference<CsvDataSort> csvDataSort = new AtomicReference<>(new CsvDataSort());
+            resultList.add(csvDataSort.get());
+            csvDataSort.get().setSpecies(key);
+            //集合
+            List<CsvDataDTO> csvDataDTOS = entry.getValue();
+            csvDataDTOS.stream()
+                    .sorted(Comparator.comparing(CsvDataDTO::getShootingLocalDateTime))
+                    .forEachOrdered(
+                            csvDataDTO -> {
+                                if (csvDataSort.get().getLastShootingLocalDateTime() == null) {
+                                    _set(csvDataSort.get(), csvDataDTO);
+                                    return;
+                                }
+                                LocalDateTime lastShootingLocalDateTime = csvDataSort.get().getLastShootingLocalDateTime();
+                                LocalDateTime shootingLocalDateTime = csvDataDTO.getShootingLocalDateTime();
+
+                                Duration duration = java.time.Duration.between(lastShootingLocalDateTime, shootingLocalDateTime);
+                                long l = Math.abs(duration.toMinutes());
+                                if (l <= 30) {
+                                    _set(csvDataSort.get(), csvDataDTO);
+                                } else {
+                                    csvDataSort.set(new CsvDataSort());
+                                    resultList.add(csvDataSort.get());
+                                    csvDataSort.get().setSpecies(key);
+                                    _set(csvDataSort.get(), csvDataDTO);
+                                }
+                            }
+                    );
+        }
         csvReader.close();
     }
 
+    private void _set(CsvDataSort csvDataSort, CsvDataDTO csvDataDTO) {
+        csvDataSort.setLastShootingLocalDateTime(csvDataDTO.getShootingLocalDateTime());
+        csvDataSort.getCsvDataDTOS().add(csvDataDTO);
+    }
+
+    private Map<String, Field> getStringFieldMap(CsvDataDTO csvDataDTO) {
+        Class<? extends CsvDataDTO> aClass = csvDataDTO.getClass();
+        Field[] declaredFields = aClass.getDeclaredFields();
+        Map<String, Field> map = new HashMap<>();
+        for (Field field : declaredFields) {
+            DataMatch annotation = field.getAnnotation(DataMatch.class);
+            if (annotation == null) continue;
+
+            String value = annotation.value();
+            map.put(value, field);
+        }
+        return map;
+    }
+
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Setter
+    @Getter
+    public class CsvDataSort {
+        private String species;
+        private LocalDateTime lastShootingLocalDateTime;
+        private final List<CsvDataDTO> csvDataDTOS = new ArrayList<>();
+    }
 }
